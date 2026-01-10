acb625dee8348dd4b5b0cb5a4af3605d32885e47
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2026-01-10 20:25:35 +0100

    feat: manually free GPU memory in Pipeline

--- notes_rag/core/pipeline.py
+++ notes_rag/core/pipeline.py
@@ -1,7 +1,11 @@
+import gc
 import sys
+
+import torch
 
 from notes_rag.core.generation import Generator, GenerationRequest
 from notes_rag.core.retrieval import Retriever, Reranker
+from notes_rag.core.schema import NoteChunk
 
 
 class Pipeline:
@@ -12,15 +16,19 @@ class Pipeline:
         self.reranker = reranker
         self.generator = generator
 
-    def answer(self, question: str):
+    @torch.no_grad()
+    def answer(self, question: str) -> tuple[str, list[NoteChunk]]:
         top_context = self.retrieve_context(question)
         print("Generuję odpowiedź...", file=sys.stderr)
         generation_request = GenerationRequest(
             user_prompt=question, context=top_context
         )
         answer = self.generator.generate(generation_request)
+        torch.cuda.empty_cache()
+        gc.collect()
         return answer, top_context
 
+    @torch.no_grad()
     def answer_stream(self, question: str, on_token=None):
         top_context = self.retrieve_context(question)
         print("Generuję odpowiedź...", file=sys.stderr)
@@ -28,9 +36,11 @@ class Pipeline:
             user_prompt=question, context=top_context
         )
         final = self.generator.generate_stream(generation_request, on_token=on_token)
+        torch.cuda.empty_cache()
+        gc.collect()
         return final, top_context
 
-    def retrieve_context(self, question: str):
+    def retrieve_context(self, question: str) -> list[NoteChunk]:
         print("Wyszukuję istotne dokumenty...", file=sys.stderr)
         candidates = []
         for retriever in self.retrievers:
